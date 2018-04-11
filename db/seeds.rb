acad9496e1893b755f6d8f40bdff94fa613e588d
Alexander <devtop000@gmail.com>
2018-04-11 11:37:58 +0300

    Add account activation

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,16 @@
 User.create!(email: 'example@example.org',
              password:              'foobar',
              password_confirmation: 'foobar',
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 20.times do |n|
   email = "example-#{n + 1}@example.org"
   password = 'password'
   User.create!(email: email,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
